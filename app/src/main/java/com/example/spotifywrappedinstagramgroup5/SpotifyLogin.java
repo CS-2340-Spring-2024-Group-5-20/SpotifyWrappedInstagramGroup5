6863553646f24ce0a7fabb7ce5ccf650340b46aa
kgawande3 <157524444+kgawande3@users.noreply.github.com>
2024-03-28 13:42:05 -0400

    Added Firebase Authentication (Login and Signup Pages)

---
+++ app/src/main/java/com/example/spotifywrappedinstagramgroup5/SpotifyLogin.java
@@ -0,0 +1,14 @@
+package com.example.spotifywrappedinstagramgroup5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotifyLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_spotify_login);
+    }
+}
