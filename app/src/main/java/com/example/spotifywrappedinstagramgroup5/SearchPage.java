62f75f75cd301f919e80553b568e484a1b6900b3
tyler <tylerte103103@gmail.com>
2024-04-08 00:23:59 -0400

    Put some more functionality towards the search bar

--- app/src/main/java/com/example/spotifywrappedinstagramgroup5/SearchPage.java
+++ app/src/main/java/com/example/spotifywrappedinstagramgroup5/SearchPage.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,12 +31,16 @@ public class SearchPage extends AppCompatActivity {
     FragmentSearchpageBinding binding; // Corrected binding class
     FirebaseAuth auth;
     FirebaseUser user;
+    EditText userSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = FragmentSearchpageBinding.inflate(getLayoutInflater()); // Corrected binding initialization
         setContentView(binding.getRoot());
+        userSearch = findViewById(R.id.search_button);
+
+
 
         // Initialize Firebase authentication
         auth = FirebaseAuth.getInstance();
@@ -60,17 +68,44 @@ public class SearchPage extends AppCompatActivity {
             }
             return true;
         });
+
+        userSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userNameSearch = String.valueOf(userSearch.getText());
+                String found;
+                searchForUser(userNameSearch, new Callback() {
+                    @Override
+                    public void onSuccess(String username) {
+//                        found = username; //do something
+                    }
+
+                    @Override
+                    public void onNotFound() {
+                        Toast.makeText(SearchPage.this, "User not found.", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        //hopefully will not throw an error
+                    }
+                });
+            }
+        });
+
+
     }
     FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
+
 
     /**
      * Method to search for profiles after user inputs profile username.
      * @param user username to search for
      * @return a single capacity string array containing the UUID of the user's profile which was searched for.
      */
-    private String[] searchForUser(String user) {
-        final String[] ret = new String[1];
-
+    private void searchForUser(String user, final Callback callback) {
         DocumentReference docRef = mStore.collection("Userdata").document(user);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -78,16 +113,24 @@ public class SearchPage extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot doc = task.getResult();
                     if (doc.exists()) {
-                        ret[0] = doc.getString("UUID");
+                        String uuid = doc.getString("Username");
+                        callback.onSuccess(uuid); // Display user in some manner
+                    } else {
+                        callback.onNotFound(); // Notify caller that user was not found
 
-                    }else {
-                        Log.d(TAG, "No such document");
                     }
                 } else {
-                    Log.d(TAG,"get failed with ",task.getException());
+                    callback.onError(task.getException()); // Notify caller of error
                 }
             }
         });
-        return ret;
     }
+
+    // Define a callback interface
+    interface Callback {
+        void onSuccess(String uuid);
+        void onNotFound();
+        void onError(Exception e);
+    }
+
 }
