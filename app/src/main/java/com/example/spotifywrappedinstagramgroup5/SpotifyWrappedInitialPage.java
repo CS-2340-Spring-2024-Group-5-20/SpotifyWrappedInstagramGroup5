5aaba99249edc399c9d4d1a21ccd3c9b32e56e33
tyler <tylerte103103@gmail.com>
2024-04-21 23:09:17 -0400

    Trying to add likes button functionality to new spotify wrap layout

--- app/src/main/java/com/example/spotifywrappedinstagramgroup5/SpotifyWrappedInitialPage.java
+++ app/src/main/java/com/example/spotifywrappedinstagramgroup5/SpotifyWrappedInitialPage.java
@@ -1,26 +1,38 @@
 package com.example.spotifywrappedinstagramgroup5;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.spotifywrappedinstagramgroup5.databinding.SpotifyWrappedInitialPageBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SpotifyWrappedInitialPage extends AppCompatActivity {
     SpotifyWrappedInitialPageBinding binding;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spotify_wrapped_initial_page);
+
 
         binding = SpotifyWrappedInitialPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -51,6 +63,14 @@ public class SpotifyWrappedInitialPage extends AppCompatActivity {
             List<String> artists = intent.getStringArrayListExtra("artists");
             List<String> genres = intent.getStringArrayListExtra("genres");
 
+
+
+                    // toggleLike("cbf8da5d-0882-4b60-aeb9-9aa77c804ab6");
+                    // MUST FIND WAY TO PERSIST POST ID INTO THIS CLASS.
+
+
+
+
             forwardButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -66,5 +86,54 @@ public class SpotifyWrappedInitialPage extends AppCompatActivity {
                 }
             });
         }
+    }
+    private void toggleLike(String postId) {
+        Context context = this;
+        FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        CollectionReference wrapsRef = mStore.collection("Wraps");
+        // create reference to Wraps collection
+        TextView likeCountTextView = findViewById(R.id.likesCountTextView);
+        wrapsRef.whereEqualTo("PostId", postId)
+                // queries for Wrap in collection with corresponding postId
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        Map<String, Object> wrapData = document.getData();
+                        // creates local hashmap of wrap document
+                        if (!wrapData.containsKey("LikedUserIds")) {
+                            //checks if LikedUserIds exists already
+                            wrapData.put("LikedIds", new ArrayList<String>());
+                            //makes LikedUserIds if it does not already exist
+                        }
+                        ArrayList<String> likedIDS = (ArrayList<String>) wrapData.get("LikedUserIds");
+                        // creates local array of liked user ids
+
+                        long likes =  (long) wrapData.get("LikeCount");
+                        // creates local number of likes
+                        if (likedIDS.contains(currentUser.getEmail())) {
+                            // checks if user already liked the wrap
+                            likes--;
+                            likedIDS.remove(currentUser.getEmail());
+                            // will decrement likes and remove user from "likes" array
+                            document.getReference().update("LikedUserIds", likedIDS);
+                            document.getReference().update("LikeCount", likes);
+                            //updates both relevant fields
+                            Toast.makeText(context.getApplicationContext(), "Unliked wrap", Toast.LENGTH_SHORT).show();
+                        } else {
+                            likes++;
+                            likedIDS.add(currentUser.getEmail());
+                            // will increment likes and add user to likes array
+                            document.getReference().update("LikedUserIds", likedIDS);
+                            document.getReference().update("LikeCount", likes);
+                            // updates both relevant fields
+                            Toast.makeText(context.getApplicationContext(), "Liked wrap", Toast.LENGTH_SHORT).show();
+
+                        }
+                        likeCountTextView.setText(String.valueOf(likes));
+
+                    }
+                });
     }
 }
